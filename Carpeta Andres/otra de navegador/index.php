f6b336ee95cabb861fae4a5dd62fa53cf43eb0c6
carlo1404 <brunoreyes150@gmail.com>
2025-05-08 17:37:13 -0500

    login usando poo

---
+++ Carpeta Andres/otra de navegador/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>log in</title>
+</head>
+<body>
+    <h1>Log in</h1>
+    <form action="login.php" method="post">
+        <label for="username">Nombre:</label><br>
+        <input type="text" id="username" name="username"><br><br>
+        <label for="password">contraseña:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
